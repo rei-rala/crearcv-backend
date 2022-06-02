5a0964285288bde3bea84c5504e470931389c2b6
rei-rala <ramonirala@outlook.com>
2022-06-02 12:07:56 -0300

    Changed CV+User models && its html form (frontend)

--- src/models/Cv.ts
+++ src/models/Cv.ts
@@ -11,25 +11,23 @@ const cvSchema = new Schema({
   about: { type: String, maxlength: [600, 'Descripcion no debe exceder los 600 caracteres'] },
   photos: [{ type: String, maxlength: [200, 'URL de imagen no debe exceder los 200 caracteres'] }],
   location: { type: String, maxlength: [100, 'Ubicacion no debe exceder los 100 caracteres'] },
-  history: {
-    academic: [{
-      title: { type: String, maxlength: [100, "Titulacion no debe exceder 100 caracteres"] },
-      where: { type: String, maxlength: [100, "Ubicacion academica no debe exceder 100 caracteres"] },
-      from: { type: Date, },
-      to: { type: Date, },
-      condition: { type: String, maxlength: [20, "Condicion no debe superar 20 caracteres"] },
-      description: { type: String, maxlength: [100, "Descripcion no debe superar 100 caracteres"] },
-      isCurrent: { type: Boolean, default: false }
-    }],
-    professional: [{
-      title: { type: String, maxlength: [100, "Titulacion no debe exceder 100 caracteres"] },
-      where: { type: String, maxlength: [100, "Ubicacion laboral no debe exceder 100 caracteres"] },
-      from: { type: Date, },
-      to: { type: Date, },
-      seniority: { type: String, maxlength: [20, "Seniority no debe exceder 20 caracteres"] },
-      isCurrent: { type: [Boolean, "Solamente verdadero o falso"], default: false },
-    }],
-  },
+  academic: [{
+    title: { type: String, maxlength: [100, "Titulacion no debe exceder 100 caracteres"] },
+    where: { type: String, maxlength: [100, "Ubicacion academica no debe exceder 100 caracteres"] },
+    from: { type: Date, },
+    to: { type: Date, },
+    condition: { type: String, maxlength: [20, "Condicion no debe superar 20 caracteres"] },
+    description: { type: String, maxlength: [100, "Descripcion no debe superar 100 caracteres"] },
+    isCurrent: { type: Boolean, default: false }
+  }],
+  professional: [{
+    title: { type: String, maxlength: [100, "Titulacion no debe exceder 100 caracteres"] },
+    where: { type: String, maxlength: [100, "Ubicacion laboral no debe exceder 100 caracteres"] },
+    from: { type: Date, },
+    to: { type: Date, },
+    seniority: { type: String, maxlength: [20, "Seniority no debe exceder 20 caracteres"] },
+    isCurrent: { type: [Boolean, "Solamente verdadero o falso"], default: false },
+  }],
   contact: [{
     type: { type: String, maxlength: [20, "Tipo de contacto no debe superar 20 caracteres"] },
     url: { type: String, maxlength: [150, "URL debe tener maximo 150 caracteres"], },
--- src/models/User.ts
+++ src/models/User.ts
@@ -4,6 +4,7 @@ const userSchema = new Schema({
   id: { type: String, required: true, unique: true },
   registered: { type: Number, required: true },
   lastConnection: { type: Number, required: true },
+  username: { type: String, match: [/^[A-Za-z0-9]+$/g, 'Nombre de usuario debe ser solo letras y numeros'], minlength: [3, 'Nombre de usuario debe tener al menos 3 caracteres'], maxlength: [30, 'Nombre de usuario no debe exceder los 30 caracteres'] },
 })
 
 const User = model('User', userSchema);
